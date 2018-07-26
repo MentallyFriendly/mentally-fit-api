ab127a022f2b456a4ccc1e3e082f3dc741f3fadc
arapl3y <rapley3@gmail.com>
2018-07-26 19:51:04 +1000

    Add user model methods and pre save hook

--- src/api/resources/user/user.model.ts
+++ src/api/resources/user/user.model.ts
@@ -1,21 +1,54 @@
 import { Document, Schema, Model, model } from 'mongoose';
 import { IUser } from '../../../interfaces/user';
+import { compareSync, genSaltSync, hashSync } from 'bcrypt';
 
-export interface IUserModel extends IUser, Document {}
+export interface IUserModel extends IUser, Document {
+  hashPassword(plainTextPassword: string): string;
+  authenticate(plainTextPassword: string): boolean;
+}
 
 const userSchema = new Schema(
   {
     email: {
       type: String,
       unique: true,
-      required: true
-    },
-    passwordHash: {
       required: true,
-      type: String
+      trim: true
+    },
+    password: {
+      type: String,
+      required: true,
+      trim: true
     }
   },
   { timestamps: true }
 );
+
+userSchema.methods = {
+  authenticate(plainTextPassword: string) {
+    return compareSync(plainTextPassword, this.password);
+  },
+  hashPassword(plainTextPassword: string) {
+    // Change these methods to ASYNC
+    if (!plainTextPassword) {
+      throw new Error('Could not save user');
+    }
+
+    const salt = genSaltSync();
+    return hashSync(plainTextPassword, salt);
+  }
+};
+
+userSchema.pre('save', function(next) {
+  const user = this;
+
+  try {
+    const hash = user.hashPassword(user.password);
+    user.password = hash;
+    next();
+  } catch (error) {
+    return next(error);
+  }
+});
 
 export const User: Model<IUserModel> = model<IUserModel>('user', userSchema);
